790a540e402a9120f91549caa2bfc16eb8eca05a
nithyashreebg <nithyagowda178@gmail.com>
2024-12-18 12:21:27 +0530

    Connection updated

--- PSEcomm/src/main/java/com/database/DBConnection.java
+++ PSEcomm/src/main/java/com/database/DBConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection
-
+{
 	static String url = "jdbc:mysql://localhost:3306/ecommerce";
 
 	static String username = "root";
