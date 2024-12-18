3029f679a070041946ec62eb476f036bb1065534
pradeepkumar-s-2001 <pradeepkurba2001@gmail.com>
2024-12-18 11:24:24 +0530

    Merge branch 'Customer1' of https://github.com/PunithB1601/PSEComm.git into Customer1

---
+++ PSEcomm/src/main/java/com/customer/dto/Requested_location.java
@@ -0,0 +1,13 @@
+package com.customer.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class Requested_location {
+private int req_id;
+private String requested_location;
+}
